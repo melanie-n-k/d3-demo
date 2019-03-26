6ca2d2cb9087048a831251978097be37989843b9
melanie-n-k <45474048+melanie-n-k@users.noreply.github.com>
2019-03-26 09:42:44 -0500

    container
    added dimensions to svg

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@
 
 //execute script when window is loaded
 window.onload = function(){
+    ///svg dimension variables
+    var width = 900, height = 500;
 
     var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //puts a new svg into the body of the html doc 
+        .append("svg") //puts a new svg into the body of the html doc
+        .attr("width", width) //assign width
+        .attr("height", height) //assign height
+        .attr("class", "container") //to use for future styling and selection
 };
