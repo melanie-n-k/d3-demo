43c92ed9a64055ed487d00ec2b2227d029d11c64
melanie-n-k <45474048+melanie-n-k@users.noreply.github.com>
2019-03-26 10:29:47 -0500

    circles
    added circles to the container!

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ window.onload = function(){
         .style("background-color","#FAAEF4"); //semicolon at end of code block
 
     //new block to add another element
-    var insideRect = container.append("rect") //put rectangle inside main container (svg) 
+    var insideRect = container.append("rect") //put rectangle inside main container (svg)
         .datum(400)
         .attr("width", function(d){
           return d*2;
@@ -26,4 +26,22 @@ window.onload = function(){
         .attr("x", 50) //horizontal offset
         .attr("y", 50) //vertical offset
         .style("fill","#FCEE94"); //fill in color of rectangle
+
+    var arrayData = [10, 20, 30, 40, 50]; //random array of data
+
+    var bubbles = container.selectAll(".bubbles") //empty selection of circles that don't yet exist
+        .data(arrayData) //feed in data from array
+        .enter() //?
+        .append("circle")
+        .attr("class", "bubbles")
+        .attr("r", function(d, i){
+          console.log("d:", d, "i:", i);
+          return d; //circle radius
+        })
+        .attr("cx", function(d, i){
+          return 70 + (i*180); //circle x position
+        })
+        .attr("cy", function(d){
+          return 450 - (d*5); // circle y position
+        });
 };
